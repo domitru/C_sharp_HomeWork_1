31336fc4587f8af4b18a9281c5509610e4487b9b
Dmitry Basov <dom.it.ru@yandex.ru>
2022-11-13 15:25:12 +0300

    add Task 002

---
+++ Task002/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 2: Напишите программу,
+             которая на вход принимает два числа и выдаёт,
+            какое число большее, а какое меньшее.*/
+            
+Console.WriteLine("Сравнниваем два числа.");
+
+Console.Write("Введите число A = ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число B = ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+if ( A > B )
+{
+    Console.WriteLine( "Максимальное число А = " + A);
+}
+else if ( A == B )
+{
+    Console.WriteLine( "Два числа равны между собой");
+}
+else
+{
+    Console.WriteLine( "Максимальное число В = " + B);
+}
+//  Ура!!! Это моя первая программа написанная самостоятельно.
