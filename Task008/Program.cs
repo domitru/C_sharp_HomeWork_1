2c79862772e6636f7c85365ae65155538b510dd4
Dmitry Basov <dom.it.ru@yandex.ru>
2022-11-14 18:08:16 +0300

    add Task 006,008

--- Task008/Program.cs
+++ Task008/Program.cs
@@ -4,13 +4,22 @@
              5 -> 2, 4
              8 -> 2, 4, 6, 8
                                  */
-Console.WriteLine("Введите число: ");
-string number = Console.ReadLine();
-int a = Convert.ToInt32(number);
-int b = 2;
 
-while ( b <= a)
+Console.WriteLine("Введите число: ");
+string z = Console.ReadLine();
+int a = Convert.ToInt32(z);
+Console.Write(a + " -> ");
+for (int i = 2; i < a-1; i=i+2)
 {
-    Console.Write( b + ", ");
-    b = b + 2;
+    Console.Write( i + ", ");
 }
+if (0==a%2)
+{
+    Console.Write(a);
+}
+else
+{
+    Console.Write(a-1);
+}
+
+// //  Ура!!! Это моя четвертая программа написанная самостоятельно.
